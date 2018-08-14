eba04c47bb7c2cd81eb01c8997872b5cd8c86b13
ThiagoVSouza <35876587+ThiagoVSouza@users.noreply.github.com>
2018-08-14 11:49:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,9 @@ var server = net.createServer(function(socket) {
         
                 // var output2 = buf2.toString('ascii');
      
-                socket.write(body1); // body1
+                var body2 = body1.replace('{}', '\')
+                    
+                socket.write(body2); // body1
                 
                 // socket.end();
         
